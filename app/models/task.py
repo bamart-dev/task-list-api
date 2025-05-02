162a90cf70e2d8e8655136262204b21fff3563a0
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-01 17:06:25 -0700

    implement Task class and helper methods

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,36 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[datetime]
+
+    def to_dict(self):
+        """Return object attributes in dictionary format.
+
+        When called on a Task object, the method returns a dictionary
+        containing key value pairs corresponding to the object's attributes.
+        """
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at,
+        }
+
+
+    @classmethod
+    def create_from_dict(cls, task_data):
+        """Create new task from dictionary items.
+
+        Instantiates and returns a new Task object with attribute values
+        derived from a provided dictionary.
+        """
+        return cls(
+            title = task_data["title"],
+            description = task_data["description"],
+            completed_at = task_data["completed_at"],
+        )
