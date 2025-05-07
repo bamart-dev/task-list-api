ec5e99ae1ce5de35b06c9baafedd9c25e8292664
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-06 22:13:47 -0700

    implemented basic CRUD routes and created route helper functions

---
+++ app/routes/route_utilities.py
@@ -0,0 +1,51 @@
+from flask import abort, make_response
+from app.db import db
+
+
+def validate_model(cls, model_id):
+    """Check given model ID and return model if ID is valid.
+
+    Returns a model matching the provided ID if ID is an integer and
+    a corresponding model exists.
+    """
+    try:
+        model_id = int(model_id)
+    except ValueError:
+        message = {"error": f"{cls.__name__} ID ({model_id}) not valid."}
+        abort(make_response(message, 400))
+
+    query = db.select(cls).where(cls.id == model_id)
+    model = db.session.scalar(query)
+
+    if not model:
+        message = {"error": f"{cls.__name__} ID ({model_id}) not found."}
+        abort(make_response(message, 404))
+
+    return model
+
+
+def create_model(cls, model_info):
+    """Create model from provided dictionary and save in database.
+
+    Creates a model using the contents of a given dictionary, commits
+    it to the database, then returns a response. Raises KeyError if
+    attributes are missing from request.
+    """
+    try:
+        model = cls.create_from_dict(model_info)
+    except KeyError as error:
+        response = {"details": "Invalid data"}
+        abort(make_response(response, 400))
+
+    db.session.add(model)
+    db.session.commit()
+
+    return {"task": model.to_dict()}, 201
+
+
+def get_all_models(cls):
+    """Retrieve all models from database."""
+    query = db.select(cls).order_by(cls.id)
+    models = db.session.scalars(query)
+
+    return [model.to_dict() for model in models]
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,52 @@
-from flask import Blueprint
+from flask import Blueprint, request, Response, abort, make_response
+from .route_utilities import validate_model, create_model, get_all_models
+from app.db import db
+from app.models.task import Task
+
+bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+
+    return create_model(Task, request_body)
+
+
+@bp.get("")
+def get_all_tasks():
+    return get_all_models(Task)
+
+
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+
+    return {"task": task.to_dict()}
+
+
+@bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+    try:
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+        task.completed_at = request_body.get("completed_at")
+    except KeyError as e:
+        response = {"details": f"Invalid request: missing {e.args[0]}"}
+        abort(make_response(response, 400))
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+@bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
