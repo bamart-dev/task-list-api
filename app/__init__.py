9bd2194b45057cf9a7a9674c8c458f53e059fb17
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-06 21:13:19 -0700

    update blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .db import db, migrate
 from .models import task
-from .routes.task_routes import tasks_bp
+from .routes.task_routes import bp
 from .models import task, goal
 import os
 
@@ -20,5 +20,5 @@ def create_app(config=None):
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    app.register_blueprint(tasks_bp)
+    app.register_blueprint(bp)
     return app
