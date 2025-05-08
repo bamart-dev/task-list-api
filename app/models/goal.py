cec9e2ed55bf4a02f136456694d1df917e2ae141
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-07 21:47:24 -0700

    implement Goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,28 @@ from ..db import db
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+
+
+    def to_dict(self):
+        """Return object attributes in dictionary format.
+
+        Returns a dictionary containing key value pairs corresponding
+        to the Goal object's attributes.
+        """
+        return {
+            "id": self.id,
+            "title": self.title,
+        }
+
+
+    @classmethod
+    def create_from_dict(cls, goal_data):
+        """Create new goal from dictionary.
+
+        Instantiates and returns a new Goal object with attributes
+        derived from the provided dictionary.
+        """
+        return cls(
+            title = goal_data["title"],
+        )
