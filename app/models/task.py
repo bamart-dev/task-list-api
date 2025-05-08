cec2e65bca88eabe3937767faccace3d0bfc3ea5
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-05-08 14:47:58 -0700

    refactor goal and task models to establish relationship; ran flask db migration/upgrade

--- app/models/task.py
+++ app/models/task.py
@@ -1,12 +1,17 @@
-from typing import Optional
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from datetime import datetime
 from ..db import db
+from typing import Optional, TYPE_CHECKING
+if TYPE_CHECKING:
+    from .goal import Goal
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
     completed_at: Mapped[Optional[datetime]]
 
     def to_dict(self):
